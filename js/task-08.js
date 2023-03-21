c3cb8f1e75190f1af7999927b53852ea27c15426
Myroslava-Benca <mashaivanova685@gmail.com>
2023-03-21 19:15:15 +0200

    change

--- js/task-08.js
+++ js/task-08.js
@@ -7,24 +7,24 @@ form.addEventListener("submit", onFormSubmit)
 function onFormSubmit(event) {
     event.preventDefault()
     
-    const formElements = event.currentTarget.elements
-
-    const email = formElements.email;
-    const password = formElements.password;
-    const formData = new FormData(event.currentTarget)
-   
+    const formElements = event.currentTarget.elements    
+    const email = formElements.email.value;
+    const password = formElements.password.value
     
 
-    if (email.value === "" || password.value === "") {
+    if (email === "" || password === "") {
         alert("This form must be completed");
     } else {
-        formData.forEach((name, value) => {
-        console.log(`onFormSubmit -> name`, name)
-        console.log(`onFormSubmit -> value`, value)
-    })
-        console.log(`The form has email ${email.value} and password ${password.value}`)
-
+            
+        const formData = {
+            email,
+            password
+        }
+        console.log(formData)
+    
     }
+    
+    
 }
 
 
